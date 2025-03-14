f23d4c37be77ab2e36e1ad79ba084b5d5e1ad61f
totiboca <totilopez92@gmail.com>
2025-03-14 00:03:10 -0300

    Corrigiendo pool is not defined

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const clientesRoutes = require("./routes/clientes");
-// borrar despues cons pool
-const pool = require('./config/db');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -12,27 +11,23 @@ const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
 app.use("/api/clientes", clientesRoutes);
 
+
+// Ruta a la carpeta de tu build
+app.use(express.static(path.join(__dirname, '../bandejas-app/build')));
+
+// Para manejar rutas tipo SPA (React Router):
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../bandejas-app/build', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 5000;
 app._router.stack.forEach((r) => {
     if (r.route && r.route.path) {
         console.log(`Ruta activa: ${r.route.stack[0].method.toUpperCase()} ${r.route.path}`);
     }
 });
-// borrar despues de la prueba
-app.get('/', (req, res) => {
-    res.send('¡Hola, estoy funcionando en Railway!');
-  });
 
-  app.get('/test-db', async (req, res) => {
-    try {
-      const [rows] = await pool.query('SELECT NOW() AS fecha');
-      res.json({ ok: true, fecha: rows[0].fecha });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ ok: false, error: err.message });
-    }
-  });
-// 
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
