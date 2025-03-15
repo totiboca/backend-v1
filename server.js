f0d1b35a12f7b42c7d0383fff3133be1540185af
totiboca <totilopez92@gmail.com>
2025-03-14 22:23:08 -0300

    Corrigiendo pool is not defined

--- server.js
+++ server.js
@@ -37,10 +37,10 @@ app._router.stack.forEach((r) => {
 });
 
 
-// app.use(express.static(path.join(__dirname, "build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 
 
